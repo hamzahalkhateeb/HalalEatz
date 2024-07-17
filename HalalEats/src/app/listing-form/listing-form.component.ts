57a6634787fd5ffdb58f82091b61cd83d7035021
hamzahalkhateeb <hamzahalkhateeb42@gmail.com>
2024-07-17 21:16:44 +0930

    added the basic layout of the restauraunt information input

--- HalalEats/src/app/listing-form/listing-form.component.ts
+++ HalalEats/src/app/listing-form/listing-form.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { HttpClient, } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-listing-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './listing-form.component.html',
   styleUrl: './listing-form.component.css'
 })
 export class ListingFormComponent {
 
+    resInfo = {
+      name: '', 
+      location: '',
+      photo: '',
+      days: {
+        monday: { open: false, openingHours: { start: '', end: '' } },
+        tuesday: { open: false, openingHours: { start: '', end: '' } },
+        wednesday: { open: false, openingHours: { start: '', end: '' } },
+        thursday: { open: false, openingHours: { start: '', end: '' } },
+        friday: { open: false, openingHours: { start: '', end: '' } },
+        saturday: { open: false, openingHours: { start: '', end: '' } },
+        sunday: { open: false, openingHours: { start: '', end: '' } }
+      
+      }
+    };
+
+    constructor(@Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient, private router: Router) {}
+
+    ngOnInit(): void {
+      if (isPlatformBrowser(this.platformId)){
+        (window as any)['submitForm'] = this.submitForm.bind(this)
+        
+      }
+    }
+
+    submitForm(data: any) {
+      console.log(this.resInfo);
+    }
 }
