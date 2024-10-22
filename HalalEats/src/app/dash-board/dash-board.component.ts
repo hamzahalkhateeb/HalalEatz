717612e64d4647bd7cf767a069042851946be321
hamzahalkhateeb <hamzahalkhateeb42@gmail.com>
2024-10-22 19:21:36 +1030

    just made model div to display restaurant!

--- HalalEats/src/app/dash-board/dash-board.component.ts
+++ HalalEats/src/app/dash-board/dash-board.component.ts
@@ -1,4 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
+import { CommonModule, isPlatformBrowser, NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -7,7 +7,7 @@ import { ListingFormComponent } from '../listing-form/listing-form.component';
 @Component({
   selector: 'app-dash-board',
   standalone: true,
-  imports: [ListingFormComponent],
+  imports: [ListingFormComponent, CommonModule],
   templateUrl: './dash-board.component.html',
   styleUrl: './dash-board.component.css'
 })
@@ -16,7 +16,16 @@ export class DashBoardComponent implements OnInit {
   longitude = 0;
   latitude = 0;
 
-  restaurantPackage = {};
+  restaurantPackage: Array<{
+       id: number;
+       name: string;
+       location: string;
+       openingHours: string;
+       halalRating: number;
+       images: string;
+       distance: number;
+
+  }> = [];
 
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient, private router: Router) {}
@@ -69,7 +78,27 @@ export class DashBoardComponent implements OnInit {
         if(data.success){
           alert(data.restaurantPackage);
 
-          this.restaurantPackage = data.restaurantPackage;
+          let recievedData = JSON.parse(data.restaurantPackage);
+          this.restaurantPackage = [];
+
+          for (let i = 0; i < recievedData.length; i++){
+
+            const restaurant = recievedData[i];
+
+            this.restaurantPackage.push({
+              id: restaurant.id ,
+              name: restaurant.name ,
+              location: restaurant.location ,
+              openingHours: restaurant.openingHours ,
+              halalRating: restaurant.halalRating ,
+              images: restaurant.images ,
+              distance: restaurant.DISTANCE 
+
+            })
+
+          }
+
+          console.log(this.restaurantPackage);
         } else {
           alert(data.message);
         }
