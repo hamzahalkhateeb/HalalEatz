db02d7e95065f630c3bbd13fea914f1a60df4a16
hamzahalkhateeb <hamzahalkhateeb42@gmail.com>
2025-04-10 18:33:58 +0930

    remote database connection setup

--- API/models/database.js
+++ API/models/database.js
@@ -1,15 +1,31 @@
 require('dotenv').config();
 const {Sequelize} = require("sequelize");
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    // For Heroku deployment
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                
+                rejectUnauthorized: false
+            }
+        }
+    });
+} else {
+
 
 //connecting the sql database through sequelize
-const sequelize = new Sequelize(process.env.db_name, 
-    process.env.db_user, 
-    process.env.db_password, {
+    const sequelize = new Sequelize(process.env.db_name, 
+        process.env.db_user, 
+        process.env.db_password, {
 
-    host: process.env.db_host,
-    dialect: process.env.db_dialect
-    
-});
+        host: process.env.db_host,
+        dialect: process.env.db_dialect
+        
+    });
+}
 
 module.exports = sequelize;
 
