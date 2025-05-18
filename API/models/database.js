9a0afd4c465eefabf59db5e414d7e844e6981791
hamzahalkhateeb <hamzahalkhateeb42@gmail.com>
2025-05-18 21:27:01 +0930

    configuring local environment to implement local kubernetes

--- API/models/database.js
+++ API/models/database.js
@@ -1,31 +1,17 @@
 require('dotenv').config();
 const {Sequelize} = require("sequelize");
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    // For Heroku deployment
-    sequelize = new Sequelize(process.env.JAWSDB_URL, {
-        dialect: 'mysql',
-        dialectOptions: {
-            ssl: {
-                
-                rejectUnauthorized: false // Allow self-signed certificates
-            }
-        }
-    });
-} else {
 
 
 //connecting the sql database through sequelize
-    const sequelize = new Sequelize(process.env.db_name, 
-        process.env.db_user, 
-        process.env.db_password, {
+const sequelize = new Sequelize(process.env.db_name, 
+    process.env.db_user, 
+    process.env.db_password, {
 
-        host: process.env.db_host,
-        dialect: process.env.db_dialect
-        
-    });
-}
+    host: process.env.db_host,
+    dialect: process.env.db_dialect
+    
+});
+
 
 module.exports = sequelize;
 
